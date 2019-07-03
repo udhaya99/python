01bf8762e322b42de5077c394a25a9f056f1b79b
karu99 <52480044+karu99@users.noreply.github.com>
2019-07-03 12:57:14 +0530

    Update three.py

--- three.py
+++ three.py
@@ -1,6 +1,10 @@
 n=input()
-list1=['a','e','i','o','u']
-if n in list1:
-  print("is a vowels")
-else:
-  print("not a  vowels")
+list1=['a','e','i','o','u','A','E','I','O','U']
+if(not n.isalnum()):
+   print("consonants")
+elif(n in list1):
+  print("vowels")
+else
+  print("invalid")
+  
+
