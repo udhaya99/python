e4bc12c9200a588630276c23f1e07c576ee34226
karu99 <52480044+karu99@users.noreply.github.com>
2019-07-06 19:16:49 +0530

    Update odd1.py

--- odd1.py
+++ odd1.py
@@ -1,7 +1,5 @@
-n,m=map(int,input().split( ))
+n,m=map(int,input().split(" "))
 for i in range(n+1,m):
   if(i%2!=0):
-    print(i,end=" ")
-  else:
-    print("no odd numbers")
+    print(i,end=' ')
     
