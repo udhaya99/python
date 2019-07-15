6de76e7d764ad2f94497925934eaf1b4e6d57ef9
udhaya99 <52540980+udhaya99@users.noreply.github.com>
2019-07-15 12:39:43 +0530

    Update pro3.py

--- pro3.py
+++ pro3.py
@@ -1,10 +1,10 @@
-a,b=input().split()
-l=len(a) if len(a)<len(b) else len(b)
-d=abs(len(a)-len(b))
-count=d
+m,n=input().split()
+l=len(m) if len(m)<len(n) else len(n)
+u=abs(len(m)-len(n))
+count=u
 for i in range(l):
-  if(len(b)==1 and b[i] in a):
+  if(len(n)==1 and n[i] in m):
     break
-  if(a[i]!=b[i]):
+  if(m[i]!=n[i]):
     count+=1
 print(count)
